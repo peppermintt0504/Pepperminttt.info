e5a7c02f18be6ec1d3b51d538818cb311e6b523d
Yooooodong <73277351+peppermintt0504@users.noreply.github.com>
2023-05-24 17:20:59 +0900

    캐로셀 기능 개발 완료
    + 고도화 필요

--- src/components/carousel/Carousel.tsx
+++ src/components/carousel/Carousel.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import "./Carousel.style.css"
 import { useDispatch } from 'react-redux';
 import { useSelector } from 'react-redux';
@@ -8,25 +8,67 @@ import {RootState} from '../../redux/reducer/index'
 const Carousel = () =>{
     const dispatch = useDispatch();
     const temp = useSelector((state:RootState)=> state.generalState);
+    const carouselRef = useRef<HTMLDivElement>(null);
     const [index, setIndex] = useState<number>(0)
+
+    const tempPhoto = [
+        'https://cdn.mmnews.co.kr/news/photo/202212/14803_13984_1144.jpg',
+        'https://photo.akmall.com/image2/goods/07/03/18/97/107031897_M_1500.jpg',
+        'https://openimage.interpark.com/goods_image_big/5/2/5/7/8101415257_l.jpg',
+
+    ]
+
+    console.log(index);
     const next = () =>{
-        setIndex((pre)=>pre+1);
+        if(!carouselRef.current)    return;
+        carouselRef.current.style.transition = 'left 0.5s';
+        carouselRef.current.style.left = '-560px';
+        setTimeout(()=>{
+            carouselRef.current!.style.transition = '';
+            setIndex((pre)=>{
+                if(pre === tempPhoto.length - 1)    return 0;
+                else    return pre + 1;
+            });
+            carouselRef.current!.style.left = '-280px';
+        },500)
+    }
+    const before = () =>{
+        if(!carouselRef.current)    return;
+        carouselRef.current.style.transition = 'left 0.5s';
+        carouselRef.current.style.left = '0px';
+        setTimeout(()=>{
+            carouselRef.current!.style.transition = '';
+            setIndex((pre)=>{
+                if(pre === 0)    return tempPhoto.length - 1;
+                else    return pre - 1;
+            });
+            carouselRef.current!.style.left = '-280px';
+        },500)
     }
 
     return (
         <div className='carouselLayout'>
-            <div className='leftBTN'><img className='BTNImage' src={require('../../asset/left-arrow.png')}/></div>
+            <div onClick={before} className='leftBTN'><img className='BTNImage' src={require('../../asset/left-arrow.png')}/></div>
             <div onClick={next} className='rightBTN'><img className='BTNImage' src={require('../../asset/right-arrow.png')}/></div>
-            <div className='photo' style={{transform : `translate(-160px,-200px) rotate(5deg)`, animationName : 'slide'}}>
-
-            </div>
+            
             <div className='photo'>
+                <div ref={carouselRef} className='carousel'>
+                    <div className='carouselImg'>
+                        <img src={index === 0 ? tempPhoto[tempPhoto.length - 1] :tempPhoto[index - 1]}></img>
+                    </div>
+                    <div className='carouselImg'>
+                        <img src={tempPhoto[index]}></img>
+                    </div>
+                    <div className='carouselImg'>
+                        <img src={index === tempPhoto.length - 1 ? tempPhoto[0] :tempPhoto[index + 1]}></img>
+                    </div>
+                    
+                </div>
+            </div>
+            <div className='leftPhoto'>
                 
             </div>
-            <div className='photo'>
-                
-            </div>
-            <div className='photo'>
+            <div className='rightPhoto'>
                 
             </div>
         </div>
