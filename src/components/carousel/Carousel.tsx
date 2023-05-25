554e34398ba32685cb8d7f5d3c8094d763c487b2
Yooooodong <73277351+peppermintt0504@users.noreply.github.com>
2023-05-25 15:50:40 +0900

    두 번째 페이지 디자인 작업

--- src/components/carousel/Carousel.tsx
+++ src/components/carousel/Carousel.tsx
@@ -8,16 +8,17 @@ import {RootState} from '../../redux/reducer/index'
 const Carousel = () =>{
     const dispatch = useDispatch();
     const temp = useSelector((state:RootState)=> state.generalState);
-    const carouselRef = useRef<HTMLDivElement>(null);
+    const carouselRef = useRef<null[] | HTMLDivElement[]>([]);
     const isMoving = useRef<Boolean>(false);
     const [reload, setReload] = useState<Boolean>(false)
     const [index, setIndex] = useState<number>(0)
 
     const tempPhoto = [
-        'https://cdn.mmnews.co.kr/news/photo/202212/14803_13984_1144.jpg',
-        'https://photo.akmall.com/image2/goods/07/03/18/97/107031897_M_1500.jpg',
-        'https://openimage.interpark.com/goods_image_big/5/2/5/7/8101415257_l.jpg',
-
+        'https://blog.kakaocdn.net/dn/yDtgP/btrJaVA4iLN/9QtVWLrCIrUtDz7U4Udsr0/img.jpg',
+        'https://mblogthumb-phinf.pstatic.net/MjAyMTAyMDRfNjIg/MDAxNjEyNDA4OTk5NDQ4.6UGs399-0EXjIUwwWsYg7o66lDb-MPOVQ-zNDy1Wnnkg.m-WZz0IKKnc5OO2mjY5dOD-0VsfpXg7WVGgds6fKwnIg.JPEG.sunny_side_up12/1612312679152%EF%BC%8D2.jpg?type=w800',
+        'https://mblogthumb-phinf.pstatic.net/MjAyMjAxMjVfMjAy/MDAxNjQzMTAyOTk2NjE0.gw_H_jjBM64svaftcnheR6-mHHlmGOyrr6htAuxPETsg.8JJSQNEA5HX2WmrshjZ-VjmJWqhmgE40Qm5csIud9VUg.JPEG.minziminzi128/IMG_7374.JPG?type=w800',
+        'https://djpms9a1go7nf.cloudfront.net/prod/uploads/thumbnail/images/10043263/167100535142741_md.png',
+        'https://image.musinsa.com/mfile_s01/2022/04/05/8e78082b9922dd076806a39073c8615c215014.jpg',
     ]
     useEffect(()=>{
         //image preload
@@ -30,30 +31,45 @@ const Carousel = () =>{
     const next = () =>{
         if(!carouselRef.current || isMoving.current)    return;
         isMoving.current = true;
-        carouselRef.current.style.transition = 'left 0.5s';
-        carouselRef.current.style.left = '-560px';
+        carouselRef.current.forEach((cur,i)=>{
+            if(!cur)    return
+            cur.style.transition = 'left 0.5s';
+            cur.style.left = '-560px';
+        })
+        
         setTimeout(()=>{
-            carouselRef.current!.style.transition = '';
+            carouselRef.current.forEach((cur,i)=>{
+                if(!cur)    return
+                cur.style.transition = '';
+            })
             setIndex((pre)=>{
                 if(pre === tempPhoto.length - 1)    return 0;
                 else    return pre + 1;
             });
             setReload(true);
-            
         },500)
         setTimeout(()=>{
-            carouselRef.current!.style.left = '-280px';
+            carouselRef.current.forEach((cur,i)=>{
+                if(!cur)    return
+                cur.style.left = '-280px';
+            })
             setReload(false);
             isMoving.current = false;
-        },510)
+        },550)
     }
     const before = () =>{
         if(!carouselRef.current || isMoving.current)    return;
         isMoving.current = true;
-        carouselRef.current.style.transition = 'left 0.5s';
-        carouselRef.current.style.left = '0px';
+        carouselRef.current.forEach((cur,i)=>{
+            if(!cur)    return
+            cur.style.transition = 'left 0.5s';
+            cur.style.left = '0px';
+        })
         setTimeout(()=>{
-            carouselRef.current!.style.transition = '';
+            carouselRef.current.forEach((cur,i)=>{
+                if(!cur)    return
+                cur.style.transition = '';
+            })
             setIndex((pre)=>{
                 if(pre === 0)    return tempPhoto.length - 1;
                 else    return pre - 1;
@@ -62,10 +78,13 @@ const Carousel = () =>{
             
         },500)
         setTimeout(()=>{
-            carouselRef.current!.style.left = '-280px';
+            carouselRef.current.forEach((cur,i)=>{
+                if(!cur)    return
+                cur.style.left = '-280px';
+            })
             setReload(false);
             isMoving.current = false;
-        },510)
+        },550)
     }
 
     return (
@@ -74,7 +93,7 @@ const Carousel = () =>{
             <div onClick={next} className='rightBTN'><img className='BTNImage' src={require('../../asset/right-arrow.png')}/></div>
             
             <div className='photo'>
-                <div ref={carouselRef} className='carousel'>
+                <div ref={(element) => carouselRef.current[0] = element} className='carousel'>
                     <div className='carouselImg'>
                         <img src={reload ? tempPhoto[index] : index === 0 ? tempPhoto[tempPhoto.length - 1] :tempPhoto[index - 1]}></img>
                     </div>
@@ -87,12 +106,40 @@ const Carousel = () =>{
                     
                 </div>
             </div>
+
             <div className='leftPhoto'>
-                
+                <div ref={(element) => carouselRef.current[1] = element} className='carousel'>
+                    <div className='carouselImg'>
+                        <img src={reload ? tempPhoto[index - 1 < 0 ? tempPhoto.length + (index - 1) :(index - 1) % tempPhoto.length] :
+                            tempPhoto[index - 2 < 0 ? tempPhoto.length + (index - 2) :(index - 2) % tempPhoto.length]
+                        }></img>
+                    </div>
+                    <div className='carouselImg'>
+                        <img src={tempPhoto[index - 1 < 0 ? tempPhoto.length + (index - 1) :(index - 1) % tempPhoto.length]}></img>
+                    </div>
+                    <div className='carouselImg'>
+                        <img src={reload ? tempPhoto[index - 1 < 0 ? tempPhoto.length + (index - 1) :(index - 1) % tempPhoto.length] : tempPhoto[index]}></img>
+                    </div>
+                    
+                </div>
             </div>
+            <div className='leftBlod'/>
+
             <div className='rightPhoto'>
-                
+                <div ref={(element) => carouselRef.current[3] = element} className='carousel'>
+                    <div className='carouselImg'>
+                        <img src={reload ? tempPhoto[(index + 1) % tempPhoto.length] : tempPhoto[index]}></img>
+                    </div>
+                    <div className='carouselImg'>
+                        <img src={tempPhoto[(index + 1) % tempPhoto.length]}></img>
+                    </div>
+                    <div className='carouselImg'>
+                        <img src={reload ? tempPhoto[(index + 1) % tempPhoto.length] : tempPhoto[(index + 2) % tempPhoto.length]}></img>
+                    </div>
+                    
+                </div>
             </div>
+            <div className='rightBold'/>
         </div>
     )
 }
--- src/components/secondPage/SecondPage.tsx
+++ src/components/secondPage/SecondPage.tsx
@@ -12,7 +12,14 @@ const SecondPage = () =>{
     
     return (
         <div className='secondPageLayout'>
-            <Carousel></Carousel>
+            <Carousel/>
+            <div className='descriptionContainer'>
+                <div className='Main1'>안녕하세요!</div>
+                <div className='Main2'>개발자 유동건입니다.</div>
+                <div className='detail'>항상 도전하는 자세로 다양한 경험에 도전하며 개발 역량을 키워나가는 개발자입니다.
+                어아아아ㅏ
+                </div>
+            </div>
         </div>
     )
 }
