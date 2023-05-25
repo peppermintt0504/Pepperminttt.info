810e7dc9099d6cb31a3b6095d85af84b5e5a90cd
Yooooodong <73277351+peppermintt0504@users.noreply.github.com>
2023-05-25 19:41:08 +0900

    네비게이터 개발 및 멘트 변경

--- src/components/firstPage/FirstPage.tsx
+++ src/components/firstPage/FirstPage.tsx
@@ -4,24 +4,50 @@ import { useDispatch } from 'react-redux';
 import { useSelector } from 'react-redux';
 import {RootState} from '../../redux/reducer/index'
 import Carousel from '../carousel/Carousel';
+import { menuChange } from '../../redux/action/menu_change';
+import { getMenu } from '../../tools/getMenu';
 
 
 const FirstPage = () =>{
     const dispatch = useDispatch();
     const temp = useSelector((state:RootState)=> state.generalState);
-    
+    const scrollDown = () => {
+        dispatch(menuChange(getMenu(1)));
+    }
     return (
         <div className='firstPageLayout'>
-            <div className='leftSection'></div>
+            <div className='leftSection'>
+                <div className='decoration'>
+                    <div className='line'>
+                        {[0,0,0,0,0].map(()=> <div className='spot'></div>)}
+                    </div>
+                    <div className='line'>
+                        {[0,0,0,0,0].map(()=> <div className='spot'></div>)}
+                    </div>
+                    <div className='line'>
+                        {[0,0,0,0,0].map(()=> <div className='spot'></div>)}
+                    </div>
+                    <div className='line'>
+                        {[0,0,0,0,0].map(()=> <div className='spot'></div>)}
+                    </div>
+                    <div className='line'>
+                        {[0,0,0,0,0].map(()=> <div className='spot'></div>)}
+                    </div>
+                </div>
+            </div>
             <div className='rightSection'>
                 <img src={require('../../asset/mainPhoto.jpg')}/>
             </div>
             <div className='MainTitle'>
-                <div className='mainTitleText'>환영합니다!</div>
+                <div className='mainTitleText'>환영합니다</div>
                 <div className='secondTitleText'>개발자 유동건의 개인 페이지입니다.</div>
                 </div>
             {/* <div className='thirdTitleText'>해당 페이지는 외부 라이브러리를 최소로 사용하여 개발하였습니다.</div> */}
-            <div className='navigator'></div>
+            <div onClick={scrollDown} className='navigator'>
+                <img src={require('../../asset/down-arrow.png')} className='downArrow'/>
+                <img src={require('../../asset/down-arrow.png')} className='downArrow'/>
+                <img src={require('../../asset/down-arrow.png')} className='downArrow'/>
+            </div>
         </div>
     )
 }
