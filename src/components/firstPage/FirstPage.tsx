8f2e85686e3126f32d72e8287b7c4f8f4da74a2f
Yooooodong <animus0504@naver.com>
2023-08-21 17:00:06 +0900

    key worning 제거

--- src/components/firstPage/FirstPage.tsx
+++ src/components/firstPage/FirstPage.tsx
@@ -1,46 +1,67 @@
-import React, { useEffect, useLayoutEffect, useState } from 'react';
-import "./FirstPage.style.css"
-import { useDispatch } from 'react-redux';
-import { useSelector } from 'react-redux';
-import {RootState} from '../../redux/reducer/index'
-import Carousel from '../carousel/Carousel';
-import { menuChange } from '../../redux/action/menu_change';
-import { getMenu } from '../../tools/getMenu';
+import React, { useEffect, useLayoutEffect, useState } from "react";
+import "./FirstPage.style.css";
+import { useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
+import { RootState } from "../../redux/reducer/index";
+import Carousel from "../carousel/Carousel";
+import { menuChange } from "../../redux/action/menu_change";
+import { getMenu } from "../../tools/getMenu";
 
-
-const FirstPage = () =>{
-    const dispatch = useDispatch();
-    const temp = useSelector((state:RootState)=> state.generalState);
-    const [init,setInit] = useState<Boolean>(false)
-    const scrollDown = () => {
-        dispatch(menuChange(getMenu(1)));
-    }
-    useLayoutEffect(()=>{setTimeout(()=>{setInit(true)},3000)},[])
-    return (
-        <div className='firstPageLayout'>
-            <div className='leftSection'>
-                <div className='decoration'>
-                    {[0,0,0,0,0].map(()=> <div className='line'>
-                        {[0,0,0,0,0].map(()=> <div className='spot'></div>)}
-                    </div>)}
-                </div>
+const FirstPage = () => {
+  const dispatch = useDispatch();
+  const temp = useSelector((state: RootState) => state.generalState);
+  const [init, setInit] = useState<Boolean>(false);
+  const scrollDown = () => {
+    dispatch(menuChange(getMenu(1)));
+  };
+  useLayoutEffect(() => {
+    setTimeout(() => {
+      setInit(true);
+    }, 3000);
+  }, []);
+  return (
+    <div className="firstPageLayout">
+      <div className="leftSection">
+        <div className="decoration">
+          {[0, 0, 0, 0, 0].map((v, i) => (
+            <div key={i + "-line" + Math.random()} className="line">
+              {[0, 0, 0, 0, 0].map((v1, i1) => (
+                <div key={i + "-spot" + Math.random()} className="spot"></div>
+              ))}
             </div>
-            <div className='rightSection'>
-                <img src={require('../../asset/mainPhoto.jpg')}/>
-            </div>
-            <div className='MainTitle'>
-                <div className='mainTitleText'>환영합니다</div>
-                <div className='secondTitleText'>개발자 유동건의 개인 페이지입니다.</div>
-                </div>
-            {/* <div className='thirdTitleText'>해당 페이지는 외부 라이브러리를 최소로 사용하여 개발하였습니다.</div> */}
-            <div style={{opacity : init ? '' : 0}} onClick={scrollDown} className='navigator'>
-                <img src={require('../../asset/down-arrow.png')} className='downArrow'/>
-                <img src={require('../../asset/down-arrow.png')} className='downArrow'/>
-                <img src={require('../../asset/down-arrow.png')} className='downArrow'/>
-            </div>
+          ))}
         </div>
-    )
-}
+      </div>
+      <div className="rightSection">
+        <img src={require("../../asset/mainPhoto.jpg")} />
+      </div>
+      <div className="MainTitle">
+        <div className="mainTitleText">환영합니다</div>
+        <div className="secondTitleText">
+          개발자 유동건의 개인 페이지입니다.
+        </div>
+      </div>
+      {/* <div className='thirdTitleText'>해당 페이지는 외부 라이브러리를 최소로 사용하여 개발하였습니다.</div> */}
+      <div
+        style={{ opacity: init ? "" : 0 }}
+        onClick={scrollDown}
+        className="navigator"
+      >
+        <img
+          src={require("../../asset/down-arrow.png")}
+          className="downArrow"
+        />
+        <img
+          src={require("../../asset/down-arrow.png")}
+          className="downArrow"
+        />
+        <img
+          src={require("../../asset/down-arrow.png")}
+          className="downArrow"
+        />
+      </div>
+    </div>
+  );
+};
 
 export default FirstPage;
-
--- src/components/thirdPage/ThirdPage.tsx
+++ src/components/thirdPage/ThirdPage.tsx
@@ -1,74 +1,95 @@
-import React, { useEffect, useState } from 'react';
-import "./ThirdPage.style.css"
-import { useDispatch } from 'react-redux';
-import { useSelector } from 'react-redux';
-import {RootState} from '../../redux/reducer/index'
-import { myWorkList } from '../../data/myWork';
+import React, { useEffect, useState } from "react";
+import "./ThirdPage.style.css";
+import { useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
+import { RootState } from "../../redux/reducer/index";
+import { myWorkList } from "../../data/myWork";
 
-const ThirdPage = ({scrollEvent}: any) =>{
-    const dispatch = useDispatch();
-    const temp = useSelector((state:RootState)=> state.generalState);
-    const [hoverIndex,setHoverIndex] = useState<number>(-1);
-    const holdScroll = () =>{
-        if(!scrollEvent.current)
-            scrollEvent.current = true;
-    }
-    const releaseScroll = () =>{
-        if(scrollEvent.current)
-            scrollEvent.current = false;
-    }
-    const hover = (index : number)=>{
-        if(hoverIndex !== index)
-            setHoverIndex(index);
-        
-    }
-    const releaseHover = ()=>{
-        setHoverIndex(-1);
-    }
-    return (
-        <div className='thirdPageLayout'>
-            {/* <div className='deco'>
+const ThirdPage = ({ scrollEvent }: any) => {
+  const dispatch = useDispatch();
+  const temp = useSelector((state: RootState) => state.generalState);
+  const [hoverIndex, setHoverIndex] = useState<number>(-1);
+  const holdScroll = () => {
+    if (!scrollEvent.current) scrollEvent.current = true;
+  };
+  const releaseScroll = () => {
+    if (scrollEvent.current) scrollEvent.current = false;
+  };
+  const hover = (index: number) => {
+    if (hoverIndex !== index) setHoverIndex(index);
+  };
+  const releaseHover = () => {
+    setHoverIndex(-1);
+  };
+  return (
+    <div className="thirdPageLayout">
+      {/* <div className='deco'>
                 {[0,0,0,0,0].map(()=>
                     <div className='line'>
                         {[0,0,0,0,0].map(()=> <div className='spot'></div>)}
                     </div>
                 )}
             </div> */}
-            <div className='Title'>
-                <div className='underLine'/>
-                <div className='titleText'>My Experience</div>
-            </div>
-            <div className='contentContainer'>
-                <div className='contentRow'>
-                    {myWorkList.map((x,i)=>{
-                        return(
-                            <div className='contentCard' >
-                            <img className='cardImage' 
-                                style={{transform : hoverIndex === i ? 'scale(1.05)' : ''}}
-                                src={require(`../../asset/${x.image}`)}/>
-                            <div className='cardHover' 
-                                style={{opacity : hoverIndex === i ? 0.9 : 0}} 
-                                onMouseOver={()=>hover(i)} 
-                                onMouseLeave={()=>releaseHover()}
-                            >
-                                <div className='title'>{x.name}</div>
-                                <div className='description'>{x.desc}</div>
-                                <div className='link'>
-                                    {x.git && <img onClick={()=>{ window.open(x.git, "_blank", "noopener, noreferrer");}} src={require("../../asset/gitIconWhite.png")}/>}
-                                    {x.notion && <img onClick={()=>{ window.open(x.notion, "_blank", "noopener, noreferrer");}} src={require("../../asset/notionIconWhite.png")}/>}
-                                    {x.link &&<img onClick={()=>{ window.open(x.link, "_blank", "noopener, noreferrer");}} src={require("../../asset/internetIconWhite.png")}/>}
-                                </div>
-                            </div>
-                        </div>
-                        )
-                    }
+      <div className="Title">
+        <div className="underLine" />
+        <div className="titleText">My Experience</div>
+      </div>
+      <div className="contentContainer">
+        <div className="contentRow">
+          {myWorkList.map((x, i) => {
+            return (
+              <div key={x + "-" + i} className="contentCard">
+                <img
+                  className="cardImage"
+                  style={{ transform: hoverIndex === i ? "scale(1.05)" : "" }}
+                  src={require(`../../asset/${x.image}`)}
+                />
+                <div
+                  className="cardHover"
+                  style={{ opacity: hoverIndex === i ? 0.9 : 0 }}
+                  onMouseOver={() => hover(i)}
+                  onMouseLeave={() => releaseHover()}
+                >
+                  <div className="title">{x.name}</div>
+                  <div className="description">{x.desc}</div>
+                  <div className="link">
+                    {x.git && (
+                      <img
+                        onClick={() => {
+                          window.open(x.git, "_blank", "noopener, noreferrer");
+                        }}
+                        src={require("../../asset/gitIconWhite.png")}
+                      />
                     )}
-                    
+                    {x.notion && (
+                      <img
+                        onClick={() => {
+                          window.open(
+                            x.notion,
+                            "_blank",
+                            "noopener, noreferrer"
+                          );
+                        }}
+                        src={require("../../asset/notionIconWhite.png")}
+                      />
+                    )}
+                    {x.link && (
+                      <img
+                        onClick={() => {
+                          window.open(x.link, "_blank", "noopener, noreferrer");
+                        }}
+                        src={require("../../asset/internetIconWhite.png")}
+                      />
+                    )}
+                  </div>
                 </div>
-            </div>  
+              </div>
+            );
+          })}
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
 export default ThirdPage;
-
